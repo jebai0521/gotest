d0a5f3ca97df468f5dda0f157675d09b9a593c91
jebai <jebai@qq.com>
2019-04-21 06:21:00 +0000

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jebai0521/gotest
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,8 @@
+ckage gotest
+
+import "fmt"
+
+// Hi 返回一个友好的问候
+func Hi(name string) string {
+	   return fmt.Sprintf("Hi, %s", name)
+   }
