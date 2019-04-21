7bc9cc14089800550159f2b96072046dc5835918
jebai <jebai@qq.com>
2019-04-21 06:26:50 +0000

    bugfix

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
-ckage gotest
+package gotest
 
 import "fmt"
 
 // Hi 返回一个友好的问候
 func Hi(name string) string {
-	   return fmt.Sprintf("Hi, %s", name)
-   }
+   return fmt.Sprintf("Hi, %s", name)
+}
